be6727264c62d7d4ababa9d52722d414325322b1
inkernelpanic <luka.gogoladze.2@btu.edu.ge>
2023-01-20 12:12:58 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Question2Fragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+
+class Question2Fragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question2, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/UserModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+data class UserModel(
+    var userId: String? = null,
+    var userMail: String? = null,
+    var userPass: String? = null,
+    var userName: String? = null
+)
